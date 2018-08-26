054e7e13c92d726c91e76428c89bce40020edac3
John S <diaz.john312@gmail.com>
2018-08-26 12:21:57 -0400

    Added custon error handlers

--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -1,7 +1,18 @@
 module ExceptionHandler
   extend ActiveSupport::Concern
 
+  # Define custom error subclasses - rescue catches `StandardErrors`
+  class AuthenticationError < StandardError; end
+  class MissingToken < StandardError; end
+  class InvalidToken < StandardError; end
+
   included do
+    # Define custom handlers
+    rescue_from ActiveRecord::RecordInvalid, with: :four_twenty_two
+    rescue_from ExceptionHandler::AuthenticationError, with: :unauthorized_request
+    rescue_from ExceptionHandler::MissingToken, with: :four_twenty_two
+    rescue_from ExceptionHandler::InvalidToken, with: :four_twenty_two
+
     rescue_from ActiveRecord::RecordNotFound do |error|
       json_response({ message: error.message }, :not_found)
     end
@@ -9,5 +20,16 @@ module ExceptionHandler
     rescue_from ActiveRecord::RecordInvalid do |error|
       json_response({ message: error.message }, :unprocessable_entity)
     end
+  end
+
+  private
+  # JSON response with message; Status code 422 - unprocessable entity
+  def four_twenty_two(e)
+    json_response({ message: e.message }, :unprocessable_entity)
+  end
+
+  # JSON response with message; Status code 401 - Unauthorized
+  def unauthorized_request(e)
+    json_response({ message: e.message }, :unauthorized)
   end
 end
