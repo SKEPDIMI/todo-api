90bc2aa3f060a1dc0db7dd14c6ee678b3f874969
John S <diaz.john312@gmail.com>
2018-08-26 14:42:31 -0400

    added missing params to expired_token_generator

--- spec/support/controller_spec_helper.rb
+++ spec/support/controller_spec_helper.rb
@@ -3,7 +3,7 @@ module ControllerSpecHelper
   def token_generator(user_id)
     JsonWebToken.encode(user_id: user_id)
   end
-  def expired_token_generator()
+  def expired_token_generator(user_id)
     JsonWebToken.encode({user_id: user_id}, Time.now.to_i - 10)
   end
   # return valid headers
